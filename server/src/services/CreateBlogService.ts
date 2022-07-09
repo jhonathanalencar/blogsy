22c4279f0dcec3a84c62ef6173969baf6282e2a1
Rykasa <jhonathan-alencar@hotmail.com>
2022-07-09 14:57:32 -0300

    create blog service updated

--- server/src/services/CreateBlogService.ts
+++ server/src/services/CreateBlogService.ts
@@ -3,8 +3,15 @@ import { Blog } from "../models/Blog";
 
 class CreateBlogService {
   async execute(name: string, userId: string){
-    if(Blog.find({name: name})){
+    const parsedName = name.toLowerCase()
+    const hasName = await Blog.findOne({ name: parsedName })
+    if(hasName){
       throw new BadRequestError(`A blog already exists with this name. Please choose another name`)
+    }
+
+    const hasBlog = await Blog.findOne({ createdBy: userId })
+    if(hasBlog){
+      throw new BadRequestError('User already has a blog')
     }
     
     const blog = await Blog.create({
