42ccec9835a66413b48838a5211a409f89bac15f
Rykasa <jhonathan-alencar@hotmail.com>
2022-07-10 16:12:19 -0300

    sort by date

--- server/src/services/GetBlogByIdService.ts
+++ server/src/services/GetBlogByIdService.ts
@@ -5,6 +5,7 @@ class GetBlogByIdService{
   async execute(blogId: string){
     const blog = await Blog.findOne({ _id: blogId })
     const posts = await Post.find({ createdAtBlog: blogId })
+      .sort({publishedAt: 'desc'})
 
     posts.map((post) => blog.posts.push(post))
 
--- server/src/services/GetBlogService.ts
+++ server/src/services/GetBlogService.ts
@@ -5,6 +5,7 @@ class GetBlogService{
   async execute(userId: string){
     const blog = await Blog.findOne({ createdBy: userId })
     const posts = await Post.find({ createdAtBlog: blog._id })
+      .sort({publishedAt: 'desc'})
 
     posts.map((post) => blog.posts.push(post))
 
